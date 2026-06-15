7c500987ad408729bda2dd53982f008aba176578
Mousaab <mossab.nm.yt@gmail.com>
2026-06-15 01:28:31 +0100

    test route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'name' => 'KidneyVision AI API',
-        'status' => 'running',
-        'version' => '1.0.0',
-    ]);
+    return "Laravel works!";
 });
